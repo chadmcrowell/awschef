09e847a1dc8d178256cb8b6a0f4ae21390353a0c
Chad Crowell <chadmcrowell@gmail.com>
2018-04-05 15:01:02 -0500

    apache install vconf chmod

--- webserver/recipes/default.rb
+++ webserver/recipes/default.rb
@@ -3,3 +3,20 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+service ‘apache’ do
+    service_name ‘httpd’
+    action [:enable, :start]
+End
+
+file ‘/etc/httpd/vhost.conf’ do
+    content ‘fake vhost file’
+    notifies :restart, ‘service[httpd]’
+end
+
+file ‘/var/www/html/index.html’ do
+  content ‘Hello World’
+  mode ‘0755’
+  owner ‘root’
+  group ‘apache’
+end
